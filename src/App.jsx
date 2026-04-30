237e6cfb418188f09efeb8b404343608a78aa0bc
charu <admin@Charus-Macbook-Pro.local>
2026-04-30 10:22:07 +0530

    fix build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import ClassSelect from "./pages/ClassSelect";
-import Attendance from "./pages/Attendance";
-import Report from "./pages/Report";
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/class" element={<ClassSelect />} />
-        <Route path="/attendance" element={<Attendance />} />
-        <Route path="/report" element={<Report />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <h1>Attendance System</h1>;
+}
 }
